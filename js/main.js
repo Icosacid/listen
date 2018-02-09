fbe24e57c5de0ec5bcfd6d39cb6110ecacc7fdc5
Alexandre Andrieux <alex@icosacid.com>
2018-02-09 08:53:36 +0100

    Base navigation and components

---
+++ js/main.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // App views
+    var HomeComponent = {
+        template: jQuery('#home-template').html(),
+        data: function() {
+            return { };
+        },
+        methods: { },
+        mounted: function() { }
+    };
+    var WallpapersComponent = {
+        template: jQuery('#wallpapers-template').html(),
+        data: function() {
+            return { }
+        }
+    };
+    var ShopComponent = {
+        template: jQuery('#shop-template').html(),
+        data: function() {
+            return { }
+        }
+    };
+    var CommissionsComponent = {
+        template: jQuery('#commissions-template').html(),
+        data: function() {
+            return { }
+        }
+    };
+    var GalleryComponent = {
+        template: jQuery('#gallery-template').html(),
+        data: function() {
+            return { }
+        }
+    };
+
+    // Custom components
+    Vue.component('logo-links', {
+        template: jQuery('#logo-links-template').html()
+    });
+
+    // App routes
+    var routes = [
+        { path: '/', component: HomeComponent },
+        { path: '/wallpapers', component: WallpapersComponent },
+        { path: '/shop', component: ShopComponent },
+        { path: '/commissions', component: CommissionsComponent },
+        { path: '/gallery', component: GalleryComponent }
+    ];
+
+    // Build router
+    var router = new VueRouter({
+        routes: routes
+    });
+
+    var app = new Vue({
+        router: router,
+        el: '#listen-website',
+        data: {
+            isOpen: false
+        }
+    });
+
+});
