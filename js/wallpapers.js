0bf426e0dfd526821f46ad307293bc1ff0893253
Alexandre Andrieux <alexandre.andrieux@simplx.fr>
2018-03-11 21:18:09 +0100

    Nice wallpaper gallery base!

--- js/wallpapers.js
+++ js/wallpapers.js
@@ -13,20 +13,20 @@ var WallpapersComponent = {
         jQuery
             .get("data/wallpapers.xml", {})
             .done(function(data) {
-
                 var wallpapers = [];
 
-                console.log(data.getElementsByTagName('image'));
                 // Get <wallpapers>'s <wallpaper>s
                 Array.prototype.slice.call(data.children[0].children).forEach(function(w) {
                     wallpapers.push({
                         title: w.getElementsByTagName('title')[0].innerHTML,
                         src: w.getElementsByTagName('image')[0].innerHTML,
                         previewSrc: w.getElementsByTagName('preview')[0].innerHTML,
+                        description: w.getElementsByTagName('description')[0].innerHTML,
                         price: w.getElementsByTagName('price')[0].innerHTML
                     });
                 });
-                 self.wallpapers = wallpapers;
+
+                self.wallpapers = wallpapers;
             });
     }
 };
